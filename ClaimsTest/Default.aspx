d8373e8f7e85abfba4016f625d5d79b3b4ae19e3
Chris Blankenship <chris@dscoduc.com>
2015-11-21 18:56:07 -0800

    Cleaned up code

---
+++ ClaimsTest/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ClaimsTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1><asp:label ID="signedIn" runat="server" /></h1>
+            <asp:PlaceHolder ID="phClaims" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ClaimsTest/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClaimsTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ClaimsPrincipal claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+
+            if (claimsPrincipal != null)
+            {
+                signedIn.Text = "You are signed in as " + HttpContext.Current.User.Identity.Name;
+                phClaims.Controls.Add(new LiteralControl("<br />"));
+                foreach (Claim claim in claimsPrincipal.Claims)
+                {
+                    string l = string.Format("<ul><li>{0}  ==  {1}</li></ul>", claim.Type, claim.Value);
+                    phClaims.Controls.Add(new LiteralControl(l));
+                }
+            }
+            else
+            {
+                signedIn.Text = "You are not signed in.";
+            }
+        }
+    }
+}
